2dc3383c4cc9a1af3ea80f9103e3724ed8f864fe
swetavsavarn1 <swetav.savarn@gamiotech.com>
2023-05-02 22:46:38 +0530

    basic fixing

--- monkey-news/src/Components/News.js
+++ monkey-news/src/Components/News.js
@@ -8,13 +8,16 @@ function News() {
     useEffect(() => {
         async function fetchData() {
             const res = await axios.get(
-                "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=fc50408656014dd8ac252dd5a7d1346b"
+                "https://newsapi.org/v2/top-headlines?country=in&apiKey=fc50408656014dd8ac252dd5a7d1346b"
             );
+            console.log(res)
             setNews(res.data.articles);
         }
         fetchData()
     }, []);
-   
+
+
+
     return (
             <div className="container my-3">
             <h2 style={{ textAlign: "center" }}>Top Headlines</h2>
@@ -23,8 +26,8 @@ function News() {
                     <div key={index} className="col-md-4">
                         <NewsItems
                             title={newsItem.title}
-                            content={newsItem.content.split('[')[0]}
-                            imageUrl={newsItem.urlToImage}
+                            content={newsItem.content?.split('[')[0]}
+                            imageUrl={newsItem.urlToImage ?? "https://static.toiimg.com/thumb/msid-99913129,width-1070,height-580,imgsize-8578,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg"}
                             url={newsItem.url}
                         />
                     </div>
