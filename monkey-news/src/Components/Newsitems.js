7cf8a059ea5368b854cf804991e7538240802836
swetavsavarn1 <swetav.savarn@gamiotech.com>
2023-05-02 13:51:38 +0530

    fetched news through api and used state to save it

--- monkey-news/src/Components/Newsitems.js
+++ monkey-news/src/Components/Newsitems.js
@@ -1,4 +1,10 @@
+import axios from "axios"
+import { useState, useEffect } from 'react'
 function NewsItems(props) {
+    const [news, setNews] = useState(null)
+    useEffect(() => {
+        axios.get('https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=fc50408656014dd8ac252dd5a7d1346b').then((res) => setNews(res.data.articles))
+    }, [])
     let { title, content, imageUrl } = props
     return (
         <div class="card" style={{ width: "18rem" }}>
