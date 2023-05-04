ba409d06bd8a9d815fcdcff762159d361bce5a89
swetavsavarn1 <swetav.savarn@gamiotech.com>
2023-05-04 21:47:16 +0530

    loader and result size added

---
+++ monkey-news/src/Components/Loading.js
@@ -0,0 +1,11 @@
+function Loading() {
+    return (
+        <div className="text-center">
+            <iframe src="https://giphy.com/embed/xTkcEQACH24SMPxIQg" width="100" height="100" frameBorder="100" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/hand-bored-waiting-xTkcEQACH24SMPxIQg"></a></p>
+        </div>
+
+    )
+
+
+}
+export default Loading
