baed044f7829b197ff5d9523abeb23fedfdb21fc
swetavsavarn1 <swetav.savarn@gamiotech.com>
2023-05-05 21:12:30 +0530

    added country and categories

--- monkey-news/src/App.js
+++ monkey-news/src/App.js
@@ -2,14 +2,28 @@
 
 import Navbar from "./Components/Navbar";
 import News from "./Components/News";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-      <News />
-    </>
+      <Routes>
+
+      <Route exact path="/" element={<News country='in'></News>} />
+        <Route exact path="/india" element={<News country='in'></News>} />
+        <Route exact path="/russia" element={<News country='rs'></News>} />
+        <Route exact path="/japan" element={<News country='jp'></News>} />
+        <Route exact path="/hongkong" element={<News country='hk'></News>} />
+        <Route exact path="/top-headlines" element={<News category='top-headlines'></News>} />
+        <Route exact path="/sports" element={<News category='sports'></News>} />
+        <Route exact path="/business" element={<News category='business'></News>} />
+        <Route exact path="/entertainment" element={<News category='entertainment'></News>} />
+
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- monkey-news/src/Components/Navbar.js
+++ monkey-news/src/Components/Navbar.js
@@ -1,18 +1,43 @@
+import { Link } from "react-router-dom";
 function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">Monkey News</a>
+                <Link className="navbar-brand" to="/">Monkey News</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="/">Home</a>
+                            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <Link className="nav-link dropdown-toggle" to="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Country
+                            </Link>
+                            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><Link className="dropdown-item" to="/india">India</Link></li>
+                                <li><Link className="dropdown-item" to="/russia">Russia</Link></li>
+                                <li><Link className="dropdown-item" to="/japan">Japan</Link></li>
+                                <li><Link className="dropdown-item" to="/hongkong">Hong-Kong</Link></li>
+
+                            </ul>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <Link className="nav-link dropdown-toggle" to="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Categories
+                            </Link>
+                            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><Link className="dropdown-item" to="/top-headlines">Top Headline</Link></li>
+                                <li><Link className="dropdown-item" to="/sports">Sports</Link></li>
+                                <li><Link className="dropdown-item" to="/business">Business</Link></li>
+                                <li><Link className="dropdown-item" to="/entertainment">Entertainment</Link></li>
+
+                            </ul>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/">About</a>
+                            <Link className="nav-link" to="/">About</Link>
                         </li>
 
                     </ul>
